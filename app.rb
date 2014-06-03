9b39ef225ebef7c83f838e17545677516aec4a9b
yuguo <fishguo321@gmail.com>
2014-06-04 01:08:49 +0800

    change color

--- app.rb
+++ app.rb
@@ -1,3 +1,5 @@
+#!/usr/bin/ruby
+
 require "RMagick"
 require "Find"
 
@@ -5,22 +7,26 @@ class Avatar
 
   # generate
   def self.go
-    back_files = []
-    front_files = []
-    back_dir = 'back-image'
+    back_txt = 'back-color.txt'
+    front_txt = 'front-color.txt'
     front_dir = 'front-image'
-    back_files = self.findFiles(back_dir)
     front_files = self.findFiles(front_dir)
 
 
     i = 400000
-    back_files.each do |back_image|
-      front_files.each do |front_image|
-         self.cover(front_image, back_image, 'tmp/'+i.to_s+'.png')
-         i = i+1
-         puts i
+
+    File.open(back_txt, 'r').each_line do |back_color|
+      File.open(front_txt, 'r').each_line do |front_color|
+        next if back_color == front_color
+        front_files.each do |front_image|
+          self.cover(front_image,front_color, back_color, 'tmp/'+i.to_s+'.png')
+          i = i+1
+          puts i
+        end
       end
     end
+
+    puts "#{i - 400000} images has been generated!"
   end
 
   # get files
@@ -41,13 +47,22 @@ class Avatar
 
 
   # cover the two image
-  def self.cover(front_filename, back_filename, new_filename)
-    backImage = Magick::Image.read(back_filename).first
-    frontImage = Magick::Image.read(front_filename).first
+  def self.cover(front_filename, front_color, back_color, new_filename)
+    backImage = Magick::Image.new(60, 60) {self.background_color = 'transparent'}
+    Magick::Draw.new.stroke('none').stroke_width(0).fill(back_color).roundrectangle(0,0, 60,60, 8, 8).draw(backImage)
+    front_image = Magick::Image.read(front_filename).first
+    front_image.alpha(Magick::ExtractAlphaChannel)
+    front_image.background_color = front_color
+    front_image.alpha(Magick::ShapeAlphaChannel)
+    # matrix = [[1, 0, 0],
+    #           [0, 1, 0],
+    #           [0, 0, 1]]
+    # new_front_image = front_image.recolor(matrix)
 
-    new_img = backImage.composite(frontImage, Magick::CenterGravity, Magick::OverCompositeOp)
-    new_img.write(new_filename)
+    new_image = backImage.composite(front_image, Magick::CenterGravity, Magick::OverCompositeOp)
+    new_image.write(new_filename)
   end
+
 end
 
 Avatar.go
